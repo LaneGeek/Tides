ba12b863cbf5250331ec4d8feae852cf017cb77e
HS <sekhah@my.lanecc.edu>
2019-11-24 18:48:22 -0800

    Finished.

--- app/src/main/java/sekhah/lane/tides/MainActivity.kt
+++ app/src/main/java/sekhah/lane/tides/MainActivity.kt
@@ -14,22 +14,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Eventually we will get this data from the XML file
-        val date = arrayOf("2019/01/01", "2019/01/01", "2019/01/01", "2019/01/01", "2019/01/02", "2019/01/02", "2019/01/02", "2019/01/02", "2019/01/03", "2019/01/03")
-        val day = arrayOf("Tue", "Tue", "Tue", "Tue", "Wed", "Wed", "Wed", "Wed", "Thu", "Thu")
-        val time = arrayOf("02:47 AM", "09:03 AM", "04:13 PM", "10:25 PM", "03:44 AM", "09:50 AM", "05:02 PM", "11:24 PM", "04:37 AM", "10:33 AM")
-        val predictionInFt = arrayOf("2.11", "7.16", "0.42", "5.13", "2.44", "7.26", "0.03", "5.36", "2.65", "7.32")
-        val predictionInCm = arrayOf("64", "218", "13", "156", "74", "221", "1", "163", "81", "223")
-        val highLow = arrayOf("L", "H", "L", "H", "L", "H", "L", "H", "L", "H")
+        // Lets use the XML parser
+        val parser = XmlPullParserHandler()
+        val inputStream = assets.open("Florence_2019_tide_predictions.xml")
+        val tidesData = parser.parse(inputStream)
 
-        // Lets use Kotlin's functional programming here
-        val predictionInInches = predictionInCm.map { x -> (x.toFloat() / 0.254).toInt().toFloat() / 10 }
-        val highLowNiceFormat = highLow.map { x -> if (x == "H") "High" else "Low"}
-        val output = Array(date.size) {""}
-        date.forEachIndexed { i, x -> output[i] = day[i] + " " + x + "\n" + time[i] + " - " + highLowNiceFormat[i]}
+        // Lets create our parallel arrays using Kotlin's functional programming
+        val date = tidesData.map { x -> x.date }
+        val day = tidesData.map { x -> x.day }
+        val time = tidesData.map { x -> x.time }
+        val predictionInCm = tidesData.map { x -> x.predictionInCm }
+        val highLow = tidesData.map { x -> x.highLow }
 
+        // Lets create calculated and formatted arrays again using Kotlin's functional programming
+        val predictionInInches = predictionInCm.map { x -> (x!!.toFloat() / 0.254).toInt().toFloat() / 10 }
+        val highLowNiceFormat = highLow.map { x -> if (x == "H") "High" else "Low" }
+        val output = Array(date.size) { "" }
+        date.forEachIndexed { i, x -> output[i] = day[i] + " " + x + "\n" + time[i] + " - " + highLowNiceFormat[i] }
+
+        // Render the ListView
         listView.adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, output)
 
+        // Pop the toast
         listView.setOnItemClickListener { _: AdapterView<*>?, _: View?, position: Int, _: Long ->
             Toast.makeText(this, "Tide Height: ${predictionInInches[position]} Inches", Toast.LENGTH_SHORT).show()
         }
---
+++ app/src/main/java/sekhah/lane/tides/TideData.kt
@@ -0,0 +1,9 @@
+package sekhah.lane.tides
+
+class TideData {
+    var date: String? = ""
+    var day: String? = ""
+    var time: String? = ""
+    var predictionInCm: String? = ""
+    var highLow: String? = ""
+}
